5b48ae54efb79c64547c5922c997437b05ece6ec
r4d10n <rakesh.peter@gmail.com>
2022-02-09 01:23:10 +0530

    first commit

---
+++ src/cherry/class/msc/usbd_msc.d
@@ -0,0 +1,26 @@
+src/cherry/class/msc/usbd_msc.o: src/cherry/class/msc/usbd_msc.c \
+ src//cherry/core/usbd_core.h src//cherry/common/usb_util.h \
+ src//cherry/common/usb_errno.h src//cherry/common/usb_list.h \
+ src//cherry/common/usb_mem.h src//cherry/common/usb_def.h \
+ src//cherry/common/usb_dc.h src/cherry/class/msc/usbd_msc.h \
+ src/cherry/class/msc/usb_msc.h src/cherry/class/msc/usb_scsi.h
+
+src//cherry/core/usbd_core.h:
+
+src//cherry/common/usb_util.h:
+
+src//cherry/common/usb_errno.h:
+
+src//cherry/common/usb_list.h:
+
+src//cherry/common/usb_mem.h:
+
+src//cherry/common/usb_def.h:
+
+src//cherry/common/usb_dc.h:
+
+src/cherry/class/msc/usbd_msc.h:
+
+src/cherry/class/msc/usb_msc.h:
+
+src/cherry/class/msc/usb_scsi.h:
